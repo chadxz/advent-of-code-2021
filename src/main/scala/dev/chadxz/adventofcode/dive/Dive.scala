e426a55a74e30cefee46828bba14d3a260ab2883
Chad McElligott <chad.mcelligott@gmail.com>
2021-12-05 23:18:05 -0600

    Day 2 Part 2

--- src/main/scala/dev/chadxz/adventofcode/dive/Dive.scala
+++ src/main/scala/dev/chadxz/adventofcode/dive/Dive.scala
@@ -1,27 +1,52 @@
 package dev.chadxz.adventofcode.dive
 
 import scala.io.Source
+import scala.util.matching.Regex
 
 object Dive {
-  def main(args: Array[String]): Unit = {
-    val Up = raw"up (\d+)".r
-    val Down = raw"down (\d+)".r
-    val Forward = raw"forward (\d+)".r
+  private val Up: Regex = raw"up (\d+)".r
+  private val Down: Regex = raw"down (\d+)".r
+  private val Forward: Regex = raw"forward (\d+)".r
 
+  def main(args: Array[String]): Unit = {
+    val movements =
+      Source
+        .fromInputStream(getClass.getResourceAsStream("input.txt"))
+        .getLines()
+        .toSeq
+
+    println(s"Part 1 result: ${part1(movements)}")
+    println(s"Part 2 result: ${part2(movements)}")
+  }
+
+  def part1(movements: Seq[String]): Int = {
     var horizontal = 0;
     var depth = 0;
 
-    Source
-      .fromInputStream(getClass.getResourceAsStream("input.txt"))
-      .getLines()
+    movements
       .foreach{
         case Forward(value) => horizontal += value.toInt
         case Up(value) => depth -= value.toInt
         case Down(value) => depth += value.toInt
       }
 
-    println(s"Horizontal is ${horizontal} and Depth is ${depth}")
-    println(s"Multiplication is ${horizontal * depth}")
+    horizontal * depth
   }
 
+  def part2(movements: Seq[String]): Int = {
+    var horizontal = 0
+    var depth = 0
+    var aim = 0
+
+    movements
+      .foreach{
+        case Forward(value) =>
+          horizontal += value.toInt
+          depth = depth + (aim * value.toInt)
+        case Up(value) => aim -= value.toInt
+        case Down(value) => aim += value.toInt
+      }
+
+    horizontal * depth
+  }
 }
