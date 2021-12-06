cda05d39e7d53dbfa8d42b2a49da903ed1caac7c
Chad McElligott <chad.mcelligott@gmail.com>
2021-12-05 21:51:19 -0600

    Day 1 part 2

--- src/main/scala/dev/chadxz/adventofcode/sonarsweep/SonarSweep.scala
+++ src/main/scala/dev/chadxz/adventofcode/sonarsweep/SonarSweep.scala
@@ -4,23 +4,28 @@ import scala.io.Source
 
 object SonarSweep {
   def main(args: Array[String]): Unit = {
-    val part1Result = part1()
+    val readings =
+      Source
+        .fromInputStream(getClass.getResourceAsStream("input.txt"))
+        .getLines()
+        .map(_.toInt)
+        .toSeq
 
-    println(s"Part 1 Answer: ${part1Result}")
+    println(s"Part 1 Answer: ${part1(readings)}")
+    println(s"Part 2 Answer: ${part1(part2(readings))}")
   }
 
-  def part1(): Int = {
-    Source
-      .fromInputStream(getClass.getResourceAsStream("input.txt"))
-      .getLines()
-      .map(_.toInt)
+  def part1(readings: Seq[Int]): Int =
+    readings
       .sliding(2)
       .count {
         case Seq(a, b) => b > a
       }
-  }
 
-  def part2(): Int = {
-    1
-  }
+  def part2(readings: Seq[Int]): Seq[Int] =
+    readings
+      .sliding(3)
+      .map(_.sum)
+      .toSeq
+
 }
