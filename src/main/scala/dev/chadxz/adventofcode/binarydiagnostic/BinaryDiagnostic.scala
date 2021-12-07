504b7980eadbbd33d72527e9ebde2e4138f1a00d
Chad McElligott <chad.mcelligott@gmail.com>
2021-12-06 21:46:02 -0600

    I copied this one
    Spent a long time reading the code, docs about foldRight, and thinking
    of how to model this. In the end it required a mental leap I hadn't
    considered: `transpose`. Never used a language with this as a helper, so
    it never occurred to me to look.

--- src/main/scala/dev/chadxz/adventofcode/binarydiagnostic/BinaryDiagnostic.scala
+++ src/main/scala/dev/chadxz/adventofcode/binarydiagnostic/BinaryDiagnostic.scala
@@ -1,7 +1,27 @@
 package dev.chadxz.adventofcode.binarydiagnostic
 
+import scala.io.Source
+
 object BinaryDiagnostic {
   def main(args: Array[String]): Unit = {
+    val binaryLines =
+      Source
+        .fromInputStream(getClass.getResourceAsStream("input.txt"))
+        .getLines()
+        .toSeq
 
+    val (_, gammaRate, epsilonRate) = binaryLines
+      .transpose
+      .map(seq => seq.count(_ == '1') > seq.length / 2)
+      .foldRight((1, 0, 0)) {
+        case (isGamma, (power, gamma, epsilon)) =>
+          if (isGamma) {
+            (power * 2, gamma + power, epsilon)
+          } else {
+            (power * 2, gamma, epsilon + power)
+          }
+      }
+
+    println(gammaRate * epsilonRate)
   }
 }
